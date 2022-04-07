6ee578878a5916844b5f940e0fda5e68875a7be7
admin <admin>
2022-04-07 17:34:12 +0000

    Saved recipe: ADMIN_PROJECT.compute_Basketball_Experience_SQL_result

--- recipes/compute_Basketball_Experience_SQL_result.sql
+++ recipes/compute_Basketball_Experience_SQL_result.sql
@@ -1,4 +1,4 @@
-SELECT Player,Year, Pos, Age
+SELECT BASKETBALLv2_basketball_experience_postgres.Player,Year, Pos, Age
   FROM "BASKETBALLv2_basketball_experience_postgres"
   WHERE has_TOT == '1';
   
